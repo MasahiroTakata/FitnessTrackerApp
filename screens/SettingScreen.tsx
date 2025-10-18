96e2c62df152b2bb0dc5a56875369db8cb7d7f04
Masahiro Takata <takata005581016@gmail.com>
2025-10-18 17:52:59 +0900

    【完了】実機で背景が黒だったのと、ヘッダーの追加

--- screens/SettingScreen.tsx
+++ screens/SettingScreen.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useRef, } from 'react';
+import React, { useEffect, useRef, useLayoutEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { useThemeStore } from '../stores/themeStore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from '../styles/commonStyles';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 const colors = ['#007AFF', '#FF3B30', '#34C759', '#FF9500'];
 
 const SettingScreen: React.FC = () => {
   const { themeColor, setThemeColor } = useThemeStore();
   const isFirstRender = useRef(true);
+  type RootStackParamList = { index: undefined; };
+  type NavigationProp = StackNavigationProp<RootStackParamList, 'index'>;
+  const navigation = useNavigation<NavigationProp>();
 
   useEffect(() => {
     if (isFirstRender.current) {
@@ -28,8 +34,26 @@ const SettingScreen: React.FC = () => {
     updateThemeColor();
   }, [themeColor]);
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTitle: () => (
+        <Text style={{ fontSize: 16, fontWeight: '700', color: '#fff' }}>
+          設定
+        </Text>
+      ),
+      headerTitleAlign: 'center',
+      headerStyle: {
+        backgroundColor: themeColor,
+        elevation: 0,
+        shadowOpacity: 0,
+      },
+      headerTintColor: '#fff',
+    });
+  }, [navigation, themeColor]);
+
   return (
-    <View style={{ flex: 1, padding: 20 }}>
+    <View style={styles.container}>
       <Text style={{ fontSize: 20, marginBottom: 20 }}>テーマカラーを選択</Text>
       <View style={{ flexDirection: 'row', flexWrap: 'wrap', gap: 10 }}>
         {colors.map((color) => (
